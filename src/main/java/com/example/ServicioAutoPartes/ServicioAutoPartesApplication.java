a164328d40bdfe2dbe5bab43600808f9601e5d05
Jefferson Peña <jefferson152530@gmail.com>
2025-09-30 20:49:53 -0500

    Primer commit - subiendo proyecto inicial

---
+++ src/main/java/com/example/ServicioAutoPartes/ServicioAutoPartesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServicioAutoPartes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioAutoPartesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioAutoPartesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ServicioAutoPartes/ServicioAutoPartesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServicioAutoPartes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAutoPartesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
