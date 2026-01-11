d96efdb42b5392856c1c0ef322a66f618e768afe
djjhonH01a <djjhon_15@hotmail.com>
2026-01-11 15:07:55 -0500

    Fix controller mappings to include /api/autopartes prefix

--- src/main/java/com/example/ServicioAutoPartes/controllers/ModeloController.java
+++ src/main/java/com/example/ServicioAutoPartes/controllers/ModeloController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/modelos")
+@RequestMapping("/api/autopartes/modelos")
 public class ModeloController {
 
     @Autowired
--- src/main/java/com/example/ServicioAutoPartes/controllers/PiezaController.java
+++ src/main/java/com/example/ServicioAutoPartes/controllers/PiezaController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/piezas")
+@RequestMapping("/api/autopartes/piezas")
 public class PiezaController {
 
     @Autowired
