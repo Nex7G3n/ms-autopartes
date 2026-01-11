bfe023bd50c1a065a82f9621f612e14b8ee08b2c
djjhonH01a <djjhon_15@hotmail.com>
2026-01-11 18:06:57 -0500

    Remove Jackson Hibernate5 imports from Application

--- src/main/java/com/example/ServicioAutoPartes/ServicioAutoPartesApplication.java
+++ src/main/java/com/example/ServicioAutoPartes/ServicioAutoPartesApplication.java
@@ -3,10 +3,6 @@ package com.example.ServicioAutoPartes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.context.annotation.Bean;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -14,20 +10,6 @@ public class ServicioAutoPartesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioAutoPartesApplication.class, args);
-	}
-
-	@Bean
-	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-		builder.modulesToInstall(Hibernate5Module.class);
-		return builder;
-	}
-
-	@Bean
-	public ObjectMapper objectMapper() {
-		return new Jackson2ObjectMapperBuilder()
-			.modulesToInstall(new Hibernate5Module())
-			.build();
 	}
 
 }
